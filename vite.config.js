94e61285c9421d463710a1b88f4ce3e2119ae4c8
n3knin <malavarn.p@gmail.com>
2025-12-16 00:40:16 +0800

    pls make it work

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,14 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/commentModal.js",
+                "resources/js/postPreview.js",
+                "resources/js/autocompleteLocation.js",
+                "resources/js/Homepage.js"
+            ],
             refresh: true, // hot reload
         }),
     ],
